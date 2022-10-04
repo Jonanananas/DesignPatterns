a2554fd1af4367ebd46f278b989e406873754462
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-10-04 19:02:43 +0300

    Remove commented out code

--- State/src/main/java/com/state/EvolutionState.java
+++ State/src/main/java/com/state/EvolutionState.java
@@ -1,10 +1,6 @@
 package com.state;
 
 abstract class EvolutionState {
-    // void evolve(Pokemon p) {
-    // p.evolve();
-    // }
-
     void battle(Pokemon p) {
     }
 
@@ -20,28 +16,4 @@ abstract class EvolutionState {
     void evolve(Pokemon c, EvolutionState s) {
         c.evolve(s);
     }
-
-    // void activeOpen(Pokemon c) {
-    // }
-
-    // void passiveOpen(Pokemon c) {
-    // }
-
-    // void close(Pokemon c) {
-    // }
-
-    // void synchronize(Pokemon c) {
-    // }
-
-    // void acknowledge(Pokemon c) {
-    // }
-
-    // void send(Pokemon c) {
-    // }
-
-    // void transmit(Pokemon c, TCPOctetStream){}
-
-    // void changeState(Pokemon c, TCPState s) {
-    // c.changeState(s);
-    // }
 }
